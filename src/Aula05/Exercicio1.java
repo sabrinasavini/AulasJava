95c525b6edc1546694425bf225eddbce93c62b28
sabrina <ssavinifras@gmail.com>
2024-09-23 17:17:56 -0300

    ..

---
+++ src/Aula05/Exercicio1.java
@@ -0,0 +1,12 @@
+package Aula05;
+
+public class Exercicio1 {
+    public static void main(String[] args) {
+        int contador = 10;
+        for (int i = contador; i >= 0; i--) {
+            System.out.println(i);
+            
+        }
+        System.out.println("boa!!!!");
+    }
+}
---
+++ src/Aula05/Exercicio2.java
@@ -0,0 +1,8 @@
+package Aula05;
+
+public class Exercicio2 {
+    public static void main(String[] args) {
+        int vamocontar2 = 2 , vamocontar4 = 4 , vamocontar6 = 6
+        while ()
+    }
+    }
