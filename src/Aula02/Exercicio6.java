fc8da81e4bbe76802e01c4443a2424fdb92ceadb
sabrina <ssavinifras@gmail.com>
2024-10-03 11:13:28 -0300

    1111

---
+++ src/Aula02/Exercicio6.java
@@ -0,0 +1,18 @@
+package Aula02;
+
+public class Exercicio6 {
+    public static void main(String[] args) {
+        int comp1 = 5;
+        int comp2 = 5;
+
+        if (comp1>comp2) {
+            System.out.println(comp1 + " É maior que " + comp2);
+
+        } else if (comp1<comp2) {
+            System.out.println(comp1 + " É menor que " + comp2);
+            
+        } else {
+            System.out.println("Os dois números são iguais");
+    }
+    }
+}
---
+++ src/Aula02/Exercicio7.java
@@ -0,0 +1,7 @@
+package Aula02;
+
+public class Exercicio7 {
+    public static void main(String[] args) {
+
+    }
+}
