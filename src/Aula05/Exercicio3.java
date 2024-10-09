1d287ee59b2ad42ba5d1f137696d82b55f207df0
sabrina <ssavinifras@gmail.com>
2024-10-09 14:53:54 -0300

    ;

---
+++ src/Aula05/Exercicio3.java
@@ -0,0 +1,14 @@
+package Aula05;
+
+public class Exercicio3 {
+    public static void main(String[] args) {
+        int num1 = 5;
+        int fat1 = 1;
+        for (int i = 1; i <= num1; i++) {
+            fat1 *= i;
+
+        }
+        System.out.println("O fatorial de " + num1 + " é: " + fat1);
+    }
+
+}
---
+++ src/Aula05/Exercicio4.java
@@ -0,0 +1,7 @@
+package Aula05;
+
+public class Exercicio4 {
+    public static void main(String[] args) {
+
+    }
+}
