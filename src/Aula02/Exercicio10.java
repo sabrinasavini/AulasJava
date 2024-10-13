8dbd999ee1a67a2fdb7cb01f49dbc53eb7dd4097
sabrina <ssavinifras@gmail.com>
2024-10-12 23:42:34 -0300

    ...

---
+++ src/Aula02/Exercicio10.java
@@ -0,0 +1,9 @@
+package Aula02;
+
+public class Exercicio10 {
+    public static void main(String[] args) {
+        short valor1 = 32000; //Casting implícito de "short" para "int"
+        int valor2 = valor1;
+        System.out.println("O valor convertido é de "+valor1+" (short) para "+valor2 + " (int)");
+    }
+}
--- src/Aula02/Exercicio7.java
+++ src/Aula02/Exercicio7.java
@@ -2,6 +2,19 @@ package Aula02;
 
 
 public class Exercicio7 {
+    public static void main(String[] args) {
+        boolean cond1 = true; // está chovendo
+        boolean cond2 = false; // está frio
+        boolean cond3 = true; // é feriado
+
+        boolean resultado1 = cond1 && cond2; // verdadeiro se está chovendo e está frio
+        boolean resultado2 = cond1 || cond3; // verdadeiro se chove ou é feriado
+
+        System.out.println("Está chovendo e faz frio " + resultado1);
+        System.out.println("Faz frio ou é feriado " + resultado2);
+
+        boolean todos = (cond1 || cond3) && !cond2;
+        System.out.println("Está chovendo, faz frio e é feriado " + todos);
+    }
 
     }
-}
